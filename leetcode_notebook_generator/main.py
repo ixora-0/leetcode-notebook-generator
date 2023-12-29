91cf47495e1241598508600a03269d0f085174a3
ixora-0 <phunglegend.2004@gmail.com>
2023-12-29 13:37:28 -0500

    feat: Add support for command line arguments.

--- leetcode_notebook_generator/main.py
+++ leetcode_notebook_generator/main.py
@@ -1,27 +1,49 @@
+import argparse
 import re
 
 from leetcode_notebook_generator.notebook_generator import generate_notebook
 from leetcode_notebook_generator.web_scraper import scrape_leetcode_problem
 
-DESTINATION_DIR = "./output/"
+DEFAULT_DESTINATION_DIR = "./output/"
+
+
+def is_leetcode_url(url):
+    # Pattern that matches with url from leetcode domain
+    url_pattern = r"(https?:\/\/(.+?\.)?leetcode\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?)"
+    return re.match(url_pattern, url)
 
 
 def main():
-    while True:
-        leetcode_url = input("Enter URL: ")
+    parser = argparse.ArgumentParser(
+        description="Generate Jupyter Notebook template from leetcode problem URL"
+    )
+    parser.add_argument("url", nargs="?", help="URL of leetcode problem")
+    parser.add_argument(
+        "-d",
+        "--destination",
+        default=DEFAULT_DESTINATION_DIR,
+        help=f"Destination directory (default: {DEFAULT_DESTINATION_DIR})",
+    )
+    args = parser.parse_args()
 
-        # Pattern that matches with url from leetcode domain
-        url_pattern = r"(https?:\/\/(.+?\.)?leetcode\.com(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?)"
-        if re.match(url_pattern, leetcode_url):
-            break
-        else:
-            print("Invalid URL. Please try again.")
+    if args.url:
+        if not is_leetcode_url(args.url):
+            print("Can't recognize URL.")
+            exit(1)
+        leetcode_url = args.url
+    else:
+        while True:
+            leetcode_url = input("Enter URL: ")
+
+            if is_leetcode_url(leetcode_url):
+                break
+            print("Can't recognize URL. Please try again.")
 
     print("Scraping from leetcode site")
     problem = scrape_leetcode_problem(leetcode_url)
 
-    print("Generating note book")
-    generate_notebook(problem, DESTINATION_DIR)
+    print("Generating notebook")
+    generate_notebook(problem, args.destination)
 
 
 if __name__ == "__main__":
